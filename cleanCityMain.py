fb93f3711ec55aa31c74a2dfa908edbf0a5fbfd6
BhaminiGit <sbhamini@gmail.com>
2022-01-29 10:58:43 -0500

    changed menue

--- cleanCityMain.py
+++ cleanCityMain.py
@@ -1,1 +1,14 @@
-from flask import Flask, url_for, render_template, redirect,g
+from flask import Flask, url_for, render_template, redirect
+
+app = Flask(__name__)
+
+neighList = {
+    "neighborhood1",
+    "neighborhood2",
+    "neighborhood3"
+}
+
+@app.route("/")
+def show_home_neighScore():
+    return render_template("neighScore.html", topNeighList = neighList)
+
